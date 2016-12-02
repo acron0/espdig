af07369dd4ee56f6637202304b04cf953598d7b9
Antony Woods <antony@mastodonc.com>
2016-12-02 09:07:24 +0000

    Add the bridge (+ move feeds to config)

--- src/espdig/system.clj
+++ src/espdig/system.clj
@@ -11,26 +11,6 @@
             [espdig.components.youtube-feeds :refer [make-youtube-feeds-checker]]
             [espdig.components.json-dumper :refer [make-json-dumper]]))
 
-(def youtube-feeds
-  [{:feed/channel   "Thooorin"
-    :feed/rss       "https://www.youtube.com/feeds/videos.xml?channel_id=UCfeeUuW7edMxF3M_cyxGT8Q"
-    :feed/title-rgx [".*"]}
-   {:feed/channel   "Drop The Bomb TV"
-    :feed/rss       "https://www.youtube.com/feeds/videos.xml?channel_id=UC-SmMElbXYS91yuDR1N0RIg"
-    :feed/title-rgx [".*"]}
-   {:feed/channel   "Richard Lewis"
-    :feed/rss      "https://www.youtube.com/feeds/videos.xml?channel_id=UCEOQ9pSmMEIqfhtCDa2JORw"
-    :feed/title-rgx [".*"]}
-   {:feed/channel   "Monte Cristo"
-    :feed/rss      "https://www.youtube.com/feeds/videos.xml?channel_id=UCZ26xCMrmYnUWYFtZr7cWCg"
-    :feed/title-rgx [".*"]}
-   {:feed/channel   "Insight on Esports"
-    :feed/rss      "https://www.youtube.com/feeds/videos.xml?channel_id=UC4AGeYoOM9lRH6-L7wNFgTQ"
-    :feed/title-rgx [".*"]}
-   {:feed/channel   "Thorin's Side"
-    :feed/rss      "https://www.youtube.com/feeds/videos.xml?channel_id=UC4rma8fFU0UmxiiVzH8RHDA"
-    :feed/title-rgx [".*"]}])
-
 (defn new-system
   [profile]
   (let [config (read-config (io/resource "config.edn") {:profile profile})]
@@ -44,7 +24,7 @@
          :db    (make-db (:db config))
          :aws   (make-aws-connection (:aws config))
          :feeds (component/using
-                 (make-youtube-feeds-checker youtube-feeds (:media config))
+                 (make-youtube-feeds-checker (:youtube-feeds config) (:media config))
                  [:db])
          :yt-dl (component/using
                  (make-youtube-downloader (:media config))
